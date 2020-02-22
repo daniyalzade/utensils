3672b7ee04ff1e5d1002ab35a68457f4d91adc9b
eytan <eytan@bozburunlabs.com>
2020-02-22 10:33:31 -0500

    fix packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 import utensils
 
 requires = []
@@ -9,15 +9,12 @@ packages = [
 
 setup(
     name='utensils',
-    author='Eytan Daniyalzade',
-    author_email='eytan85@gmail.com',
-    url='http://daniyalzade.com',
+    version='0.0.43',
     packages=packages,
     description='Library of utilities for python',
     license='BSD',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    version='0.0.43',
     package_dir={
         'utensils': 'utensils'
         },
